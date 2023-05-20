e283fef77cb1a96f2f15f8fd4c29f9b5a6dde5f3
danielPomo <danielnicolaspomo@gmail.com>
2023-05-19 21:34:57 -0300

    changes

--- src/controllers/todos.controllers.js
+++ src/controllers/todos.controllers.js
@@ -4,8 +4,8 @@ const Users = require('../models/users.model')
 
 const createTodos = async (req, res) => {
     try {
-        const {title, categoriesId, usersId} = req.body
-        await Todos.create({title: title, categories_id: categoriesId, users_id: usersId})
+        const newTodo = req.body
+        await Todos.create(newTodo)
         res.status(201).send() 
     } catch (error) {
         res.status(400).json(error)
